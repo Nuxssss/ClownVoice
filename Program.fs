01230c19999649fada02663ad250a5e49df21d43
Zuzuk-null <dev.nul1@ya.ru>
2021-02-09 22:15:00 +0700

    last(maybe) commit

--- Program.fs
+++ Program.fs
@@ -1,13 +1,46 @@
-// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
-
+open Funogram
 open System
+open Funogram.Telegram.Bot
+open Funogram.Telegram.Api
+open Telegram.Types
+open ExtCore.Control
+open System.Text.Json
+open System.Net
 
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+let random = Random()
 
-[<EntryPoint>]
-let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
-    0 // return an integer exit code
+let rand() = random.Next Int32.MaxValue
+
+let client = new WebClient()
+
+let getClowns() =
+    let request = "https://raw.githubusercontent.com/IMACULGY/pleaseclown.me/master/audio.json"
+    let response = client.DownloadString request
+    JsonSerializer.Deserialize<string array>(response)
+
+let getClownAudio title =
+    Voice { 
+        Id = (string <| rand())
+        Caption = None
+        ReplyMarkup = None
+        InputMessageContent = None
+        Title = title
+        ParseMode = None
+        VoiceUrl = $"https://api.pleaseclown.me/audio/{title}?format=ogg"
+        VoiceDuration = None
+    }
+
+let onUpdate (context: UpdateContext) =
+    maybe {
+        let! query = context.Update.InlineQuery
+        query.Query |> printfn "%A"
+        let requests = getClowns() |> Array.map getClownAudio
+        answerInlineQueryBase (query.Id) requests None None None None None 
+        |> Api.api context.Config
+        |> Async.Ignore
+        |> Async.Start
+    } |> ignore
+
+startBot { defaultConfig with Token = "Здесь должна быть ваша реклама" } onUpdate None
+|> Async.RunSynchronously
+
