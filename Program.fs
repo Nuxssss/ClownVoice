9dc380611736d939f72a60b97ca5ba01005db626
Daniel <dev.nul1@ya.ru>
2021-02-10 18:22:55 +0700

    Refactoring

--- Program.fs
+++ Program.fs
@@ -1,11 +1,27 @@
 open Funogram
 open System
+open Funogram
+open System
 open Funogram.Telegram.Bot
 open Funogram.Telegram.Api
 open Telegram.Types
 open ExtCore.Control
 open System.Text.Json
 open System.Net
+
+let emptyInlineVoiceQuery = {
+    Id = ""
+    Caption = None
+    ReplyMarkup = None
+    InputMessageContent = None
+    Title = ""
+    ParseMode = None
+    VoiceUrl = ""
+    VoiceDuration = None
+}
+
+let answerInlineQuery id requests = 
+    answerInlineQueryBase id requests None None None None None
 
 let random = Random()
 
@@ -14,28 +30,21 @@ let rand() = random.Next Int32.MaxValue
 let client = new WebClient()
 
 let getClowns() =
-    let request = "https://raw.githubusercontent.com/IMACULGY/pleaseclown.me/master/audio.json"
-    let response = client.DownloadString request
-    JsonSerializer.Deserialize<string array>(response)
-
-let getClownAudio title =
-    Voice { 
-        Id = (string <| rand())
-        Caption = None
-        ReplyMarkup = None
-        InputMessageContent = None
-        Title = title
-        ParseMode = None
-        VoiceUrl = $"https://api.pleaseclown.me/audio/{title}?format=ogg"
-        VoiceDuration = None
-    }
+    "https://raw.githubusercontent.com/IMACULGY/pleaseclown.me/master/audio.json"
+    |> client.DownloadString
+    |> JsonSerializer.Deserialize<string array>
+    
+let getClownAudio title = 
+    Voice { emptyInlineVoiceQuery with 
+                Id = string <| rand()
+                Title = title
+                VoiceUrl = $"https://api.pleaseclown.me/audio/{title}?format=mp3" }
 
 let onUpdate (context: UpdateContext) =
     maybe {
         let! query = context.Update.InlineQuery
-        query.Query |> printfn "%A"
         let requests = getClowns() |> Array.map getClownAudio
-        answerInlineQueryBase (query.Id) requests None None None None None 
+        answerInlineQuery (query.Id) requests
         |> Api.api context.Config
         |> Async.Ignore
         |> Async.Start
@@ -43,4 +52,3 @@ let onUpdate (context: UpdateContext) =
 
 startBot { defaultConfig with Token = "Здесь могла быть ваша реклама" } onUpdate None
 |> Async.RunSynchronously
-
